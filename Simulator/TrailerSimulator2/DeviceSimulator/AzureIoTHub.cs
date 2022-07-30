4fbaa4110dceeccbeefeb25bba391061b51950f4
Ashish Kumar Pathak <69412333+ashishpathak0933@users.noreply.github.com>
2022-07-30 18:06:15 +0200

    initial commit

---
+++ Simulator/TrailerSimulator2/DeviceSimulator/AzureIoTHub.cs
@@ -0,0 +1,134 @@
+using Azure.Messaging.EventHubs.Consumer;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Common.Exceptions;
+using System;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DeviceSimulator
+{
+    public static class AzureIoTHub
+    {
+        private static int taskDelay = 10 * 1000;
+       
+        private static string hubName = "FleetManagerIOT";
+        private static string hubSharedAccessKey = "dZHmLzcwFxZkKAarqyMzbGTzIdCV64OYQcZpQiB21fQ"; //
+        private static string device1Name = "trailer_sensor_11111101";
+        private static string device1SharedAccessKey = "fbZeyljLdJIdF+xcepfaJt8XIXb01Hx4pRE+/PQBPGw=";
+        private static string device2Name = "trailer_sensor_22222201";
+        private static string device2SharedAccessKey = "smmh81ibtEAPyo3pGVW6oWIjl3Q0x9OXgNk/fJ89d14=";
+        private static string device3Name = "trailer_sensor_33333301";
+        private static string device3SharedAccessKey = "1LjS3M3acFAoLvhcZb52W6AY96mv3TXXs33dEzKV/c8=";
+
+       
+        private static string iotHubConnectionString = @$"HostName={hubName}.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey={hubSharedAccessKey}";
+
+        private static string deviceConnectionString1 = $"HostName={hubName}.azure-devices.net;DeviceId={device1Name};SharedAccessKey={device1SharedAccessKey}";
+        private static string deviceConnectionString2 = $"HostName={hubName}.azure-devices.net;DeviceId={device2Name};SharedAccessKey={device2SharedAccessKey}";
+        private static string deviceConnectionString3 = $"HostName={hubName}.azure-devices.net;DeviceId={device3Name};SharedAccessKey={device3SharedAccessKey}";
+        
+        
+        private static string deviceConnectionString = deviceConnectionString2;
+        private static string deviceId = device2Name;
+
+        public static async Task<string> CreateDeviceIdentityAsync(string deviceName)
+        {
+            var registryManager = RegistryManager.CreateFromConnectionString(iotHubConnectionString);
+            var device = new Device(deviceName);
+            try
+            {
+                device = await registryManager.AddDeviceAsync(device);
+            }
+            catch (DeviceAlreadyExistsException)
+            {
+                device = await registryManager.GetDeviceAsync(deviceName);
+            }
+
+            return device.Authentication.SymmetricKey.PrimaryKey;
+        }
+
+        public static async Task SendDeviceToCloudMessageAsync(CancellationToken cancelToken)
+        {
+            var deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString);
+
+            double avgTemperature = 2.0D;
+            var rand = new Random();
+
+            while (!cancelToken.IsCancellationRequested)
+            {
+                double currentTemperatureLocal = avgTemperature + rand.NextDouble() * 4;
+
+                if (DateTime.Now.Millisecond % 2 == 0)
+                {
+                    currentTemperatureLocal = 37.0D;
+                }
+
+                var telemetryDataPoint = new TrailerTelemetry
+                {
+                    id = deviceId,
+                    temperature = currentTemperatureLocal,
+                    temperatureAlert = currentTemperatureLocal > 30.0D
+                };
+                var messageString = JsonSerializer.Serialize(telemetryDataPoint);
+
+                var message = new Microsoft.Azure.Devices.Client.Message(Encoding.UTF8.GetBytes(messageString))
+                {
+                    ContentType = "application/json",
+                    ContentEncoding = "utf-8"
+                };
+                await deviceClient.SendEventAsync(message);
+                Console.WriteLine($"{DateTime.Now} > Sending message: {messageString}");
+                
+               
+                await Task.Delay(taskDelay);
+            }
+        }
+
+        public static async Task<string> ReceiveCloudToDeviceMessageAsync()
+        {
+            var oneSecond = TimeSpan.FromSeconds(1);
+            var deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString);
+
+            while (true)
+            {
+                var receivedMessage = await deviceClient.ReceiveAsync();
+                if (receivedMessage == null)
+                {
+                    await Task.Delay(oneSecond);
+                    continue;
+                }
+
+                var messageData = Encoding.ASCII.GetString(receivedMessage.GetBytes());
+                await deviceClient.CompleteAsync(receivedMessage);
+                return messageData;
+            }
+        }
+
+        public static async Task ReceiveMessagesFromDeviceAsync(CancellationToken cancelToken)
+        {
+            try
+            {
+                string eventHubConnectionString = await IotHubConnection.GetEventHubsConnectionStringAsync(iotHubConnectionString);
+                await using var consumerClient = new EventHubConsumerClient(
+                    EventHubConsumerClient.DefaultConsumerGroupName,
+                    eventHubConnectionString);
+
+                await foreach (PartitionEvent partitionEvent in consumerClient.ReadEventsAsync(cancelToken))
+                {
+                    if (partitionEvent.Data == null) continue;
+
+                    string data = Encoding.UTF8.GetString(partitionEvent.Data.Body.ToArray());
+                    Console.WriteLine($"Message received. Partition: {partitionEvent.Partition.PartitionId} Data: '{data}'");
+                }
+            }
+            catch (TaskCanceledException) { } // do nothing
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading event: {ex}");
+            }
+        }
+    }
+}
